04a026ab11609b7dd477663ddeb7f3a243543e3f
Benga <benga.olasebikan@gmail.com>
2021-10-14 23:14:30 +0100

    sanity setup and dog schema created

---
+++ dogstudio/schemas/dogs.js
@@ -0,0 +1,44 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  name: 'dog',
+  title: 'Dog',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Dogs name',
+      type: 'string'
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true
+      }
+    },
+    {
+      name: 'breed',
+      title: 'Dogs breed',
+      type: 'string'
+    },
+    {
+      name: 'age',
+      title: 'Dogs age',
+      type: 'number'
+    },
+    {
+      name: 'bio',
+      title: 'Dogs bio',
+      type: 'array',
+      of : [
+        {
+          title: 'Block',
+          type: 'block',
+          styles: [{title: 'Normal', value: 'normal'}],
+          lists: []
+        }
+      ]
+    }
+  ]
+}
